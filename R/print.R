e603cd8f84e16f59d0971d368b6eff13db68437e
Mark-Eis <mark@markeisler.com>
2024-01-26 15:35:38 +0000

    edit print.R; issue #41

--- R/print.R
+++ R/print.R
@@ -127,7 +127,7 @@ print.contingency_table <- function(x, width = NULL, ..., n = NULL, max_extra_co
 print.odds_ratio <- function(x, width = NULL, ..., n = NULL, max_extra_cols = NULL, max_footer_lines = NULL) {
     contr <- (x %@% glm)$contrasts
     has_contr <- !is.null(contr)
-    x["p_val"] <- round(x["p_val"], digits = 6)
+    x$p_val <- round(x["p_val"], digits = 7)
     NextMethod()
     if (x %@% "print_contr") {
         if (has_contr)
